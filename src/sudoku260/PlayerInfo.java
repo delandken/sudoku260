cd0694fb0da8e94f9e581a86dff45de9c64fcf37
ashleyevans1 <ashleyk.evans@hotmail.com>
2014-06-19 21:20:28 -0600

    Merge origin/master
    Conflicts:
    	src/sudoku260/AverageScore.java

---
+++ src/sudoku260/PlayerInfo.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sudoku260;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author AshleyFaust
+ */
+public class PlayerInfo implements Serializable{
+    private int HighScore;
+    private int NickName;
+   
+    public int getHighScore() {
+        return HighScore;
+    }    
+    public void setHighScore(int HighScore) {
+        this.HighScore = HighScore;
+    }
+    
+    public int getNickName() {
+        return NickName;
+    }
+
+    public void setNickName(int NickName) {
+        this.NickName = NickName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.HighScore;
+        hash = 47 * hash + this.NickName;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PlayerInfo other = (PlayerInfo) obj;
+        if (this.HighScore != other.HighScore) {
+            return false;
+        }
+        if (this.NickName != other.NickName) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
+
